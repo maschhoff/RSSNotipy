02a79384c167670571af86441fa02d1131963ffc
Mathias Aschhoff <web@little-devil.de>
2020-01-26 21:11:09 +0100

    Update rssreader.py
    Cleanup

--- rssreader.py
+++ rssreader.py
@@ -19,19 +19,6 @@ def getRSSTitles(url):
     movie={}
     
     for item in feed["items"]:
-        #print(item["title"])
-        #print(item["link"])
         movie[item["title"]]=item["link"]
-        #liste.append(movie)
 
     return movie
-
-"""
-if __name__ == '__main__':
-    print(getRSSTitles("https://www.xrel.to/feeds/atom/releases.xml"))
-
-"""
-
-#https://www.xrel.to/feeds/atom/releases.xml
-#https://www.xrel.to/feeds/atom/releases-movie-topmovie.xml'
-#https://fileleechers.info/board/forum/index.php?board-feed/819/&at=8048-af1e897cd2b5bd61600abfc71bb8237e88bfa793'
