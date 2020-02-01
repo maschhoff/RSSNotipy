760077b7c067517e2e0d7c2de36b4e5558eca36d
Mathias Aschhoff <web@little-devil.de>
2020-02-01 18:10:16 +0100

    Add files via upload

--- noti.py
+++ noti.py
@@ -19,7 +19,7 @@ config=settings.loadConfig()
 message="New movie found: "
 
 def notifyPushover(movie):
-    if config.get("pushover_app_token") is not None or config.get("pushover_app_token")=="":
+    if config.get("pushover_app_token") is not None and config.get("pushover_app_token")!="":
         conn = http.client.HTTPSConnection("api.pushover.net:443")
         conn.request("POST", "/1/messages.json",
         urllib.parse.urlencode({
@@ -31,13 +31,13 @@ def notifyPushover(movie):
 
 #Notification to the String-item RSSNotipy
 def notifyOpenHAB(movie):
-    if config.get("openhab_ip") is not None or config.get("openhab_ip")=="" and config.get("openhab_port") is not None or config.get("openhab_port")=="":
+    if config.get("openhab_ip") is not None and config.get("openhab_ip")!="" and config.get("openhab_port") is not None and config.get("openhab_port")!="":
         url = "http://"+config["openhab_ip"]+":"+config["openhab_port"]+"/rest/items/RSSNotipy"
         headers = {'Content-type': 'text/plain'}
         requests.post(url, data=message+" "+movie, headers=headers)
 
 def notifyPushbullet(movie):
-    if config.get("pushbullet_api_key") is not None or config.get("pushbullet_api_key")=="":
+    if config.get("pushbullet_api_key") is not None and config.get("pushbullet_api_key")!="":
         data_send = {"type": "note", "title": "RSSNotipy", "body": message+""+movie}
         requests.post('https://api.pushbullet.com/v2/pushes', data=json.dumps(data_send),
                             headers={'Authorization': 'Bearer ' + config["pushbullet_api_key"], 'Content-Type': 'application/json'})
@@ -50,4 +50,3 @@ def notifyAll(movie):
     except:
         print("Error while sending notifications...")
 
-notifyAll("TEST")
